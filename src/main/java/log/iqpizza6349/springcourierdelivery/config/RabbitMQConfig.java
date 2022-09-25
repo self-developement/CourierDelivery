bb101f778be630ddf5d7ff8a47a17c078e60157d
iqpizza6349 <iqpizza6349@gmail.com>
2022-09-25 17:47:17 +0900

    :wrench: :: rabbitmq configuration

---
+++ src/main/java/log/iqpizza6349/springcourierdelivery/config/RabbitMQConfig.java
@@ -0,0 +1,41 @@
+package log.iqpizza6349.springcourierdelivery.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    private static final String EXCHANGE_NAME = "spring.courier-delivery";
+    private static final String QUEUE_NAME = "spring.item-queue";
+    private static final String ROUTING_KEY = "spring.courier-delivery.#";
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
