4ddfc1afab4f9c9527eaf551df82c44bdf1fa82e
iqpizza6349 <iqpizza6349@gmail.com>
2022-09-27 11:02:12 +0900

    :heavy_plus_sign: :: spring batch dependency

--- src/main/java/log/iqpizza6349/springcourierdelivery/SpringCourierDeliveryApplication.java
+++ src/main/java/log/iqpizza6349/springcourierdelivery/SpringCourierDeliveryApplication.java
@@ -1,9 +1,11 @@
 package log.iqpizza6349.springcourierdelivery;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringCourierDeliveryApplication {
 
 	public static void main(String[] args) {
